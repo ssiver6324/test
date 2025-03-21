af36705ad848e67afe54ab516c3e394d992a3dad
ssiver6324 <ssiver6324@gmail.com>
2025-03-21 09:50:51 +0900

    Update 10000hours.js

--- Basic Resouce/js/10000hours.js
+++ Basic Resouce/js/10000hours.js
@@ -54,17 +54,11 @@ window.onclick = function (event) {
 };
 
 function copyUrl() {
-    let url = window.location.href;
-    let tmp = document.createElement('input');
+    const url = window.location.href;
     
-
-    document.body.appendChild(tmp);
-    tmp.value = url;
-    tmp.select();
-	document.execCommand("copy");
-    document.body.removeChild(tmp);
-    
+    navigator.clipboard.writeText(url).then(() => {
 	alert("URL이 복사되었습니다"); 
+    })
 }
 
 shareButton.addEventListener('click', copyUrl);
